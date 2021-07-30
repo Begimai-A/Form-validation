07e4afa21db677a23016e3273229542ba0f4bfad
Begimai <begimai.akulova@gmail.com>
2021-07-30 03:03:22 +0300

    created variables with dom and functions

---
+++ script.js
@@ -0,0 +1,12 @@
+/* eslint-disable indent */
+
+const form=document.querySelector("#form"),
+	username=document.querySelector("#username"),
+	email=document.querySelector("#email"),
+	password=document.querySelector("#password");
+    password2=document.querySelector("#password2"),
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+});
+    
